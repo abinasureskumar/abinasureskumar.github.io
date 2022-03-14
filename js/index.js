bcd6f7f8b248f977e025c89b9568bd965f480a48
Sureskumar Abina, GHR-OSA-NEX-14 <Abina.Sureskumar@swisscom.com>
2022-03-14 09:49:51 +0100

    update vers.2.7

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
 // scroll to top button
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-// window.onresize = function () { idk() };
+window.onscroll = function () { scrollEvent() };
+window.addEventListener('mouseup', function (event) {
+    closeNav();
+})
 
-
-function scrollFunction() {
-    console.log(document.documentElement.scrollTop)
+function scrollEvent() {
     //Get the button
     var mybutton = document.getElementById("up");
-    if (document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
+    if (document.documentElement.scrollTop >= 20) {
+        if (mybutton.style.display == "none") {
+            mybutton.style.display = "block";
+            // console.log(document.documentElement.scrollTop)
+        }
     } else {
         mybutton.style.display = "none";
     }
 }
+
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -27,13 +31,13 @@ function topFunction() {
 
 // sidenav
 function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("Sidenav").style.width = "200px";
 }
 
 // when clicked outside + on closebtn then close nav (now only closes when clicked on button)
 // when nav is shown then click on outside close,but only when nav is shown otherwise not
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("Sidenav").style.width = "0";
 }
 
 
